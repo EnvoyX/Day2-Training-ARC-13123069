a5afd807cd685ca97d5ef425c555717ffbfb11f2
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-03-01 00:07:30 +0700

    add :  added styled tags, and some minor styling on product card

--- script.js
+++ script.js
@@ -44,30 +44,32 @@ const getProducts = async () => {
   return data.products;
 };
 
-// const renderTags = (item) => {
-//   let tags = ``;
-//   item.forEach((tag) => {
-//     return `<div>${tag}</div>`;
-//   });
-// };
-
 const renderProduct = (product) => {
   return `
     <div class="product">
-      <div class="product-img-container">
-        <img class="product-img" src=${product.images[0]} alt=${product.title} />
+    <div class="product-img-container">
+        <img class="product-img" src=${product.images[0]} alt=${
+    product.title
+  } />
       </div>
       <h3 class="product-title">${product.title}</h3>
       <div class="product-info">
       <p class="product-brand">${product.brand}</p>
-      <p class="product-category">${product.category}</p>
+      <div class="container-tags">
+         ${product.tags
+           .map((tag) => `<div class="product-tag">${tag}</div>`)
+           .join('')}
+        </div>
       </div>
       <p class="product-desc">${product.description}</p>
       <div class="product-detail">
-        <p class="product-price">$<span class="product-nominal">${product.price}</span>
-        </p>
-        <p class="product-rating">${product.rating}/5</p>
+      <p class="product-price">$<span class="product-nominal">${
+        product.price
+      }</span>
+      </p>
+      <p class="product-rating">${product.rating}/5</p>
       </div>
+        
       </div>`;
 };
 
