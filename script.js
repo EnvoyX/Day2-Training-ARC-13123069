58a0d618a2a104b970c85857300429296555669b
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-02-28 22:27:28 +0700

    feat: added navbar & footer

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+//  Hamburger menu
+const navbarNav = document.querySelector(`.navbar-nav`);
+document.querySelector(`#hamburger-menu`).onclick = () => {
+  navbarNav.classList.toggle(`active`);
+};
+
+// Click anywhere outside the element to close
+const BtnHamburger = document.querySelector(`#hamburger-menu`);
+const BtnSearch = document.querySelector(`#search-button`);
+const BtnShopping = document.querySelector('#shopping-cart-button');
+
+document.addEventListener(`click`, function (event) {
+  if (
+    !BtnHamburger.contains(event.target) &&
+    !navbarNav.contains(event.target)
+  ) {
+    navbarNav.classList.remove(`active`);
+  }
+  if (!BtnSearch.contains(event.target) && !searchForm.contains(event.target)) {
+    searchForm.classList.remove(`active`);
+  }
+  if (
+    !BtnShopping.contains(event.target) &&
+    !shoppingCartModal.contains(event.target)
+  ) {
+    shoppingCartModal.classList.remove(`active`);
+  }
+});
+
+//  For Search Bar
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector(`#search-box`);
+
+document.querySelector(`#search-button`).onclick = (event) => {
+  searchForm.classList.toggle(`active`);
+  searchBox.focus();
+};
+
 const getProducts = async () => {
   const response = await fetch(`https://dummyjson.com/products`);
   const data = await response.json();
